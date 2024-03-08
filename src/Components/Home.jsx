703c01f925cf91d3d39393a59e2be9052060b23c
Mansi Khod <98010341+MansiKhod@users.noreply.github.com>
2024-03-08 17:04:20 +0000

    background images are added

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,9 +20,9 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/woman-with-tablet.jpg";
+import image from "../images/Premium Vector _ Vector technology design on dark blue color background_.jpeg";
 
-const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
+const imageAltText = "Blue background";
 
 const Home = ({ name, title }) => {
   return (
